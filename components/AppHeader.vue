ac4b7eb6e0a6d894b00bd7023cef5cac0051d08b
phearak <phearak@pointerasia.com>
2024-01-28 20:21:58 +0700

    Modify

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -17,7 +17,7 @@
                 </ul>
                 <div class="flex items-center nav-search-box">
                     <input type="text" class="border p-3 px-5 navbar-search" placeholder="Looking for ...">
-                    <button class="border-t border-r border-b p-2.5">
+                    <button class="border-t border-black border-r border-b p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
                     </button>
                 </div>
